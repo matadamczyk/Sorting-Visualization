10d452ba8182c8da798a4a6d89935abae264f778
Mateusz Adamczyk <matieusz@hotmail.com>
2025-01-08 22:20:53 +0100

    fix: added server fixes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 import authRoutes from "./routes/auth.js";
 import apiRoutes from "./routes/api.js";
@@ -14,7 +15,27 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("views"));
+
+app.use(helmet());
+
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'", "http://localhost:8080"],
+//       scriptSrc: ["'self'", "'unsafe-inline'"],
+//       styleSrc: ["'self'", "'unsafe-inline'"],
+//       imgSrc: ["'self'", "data:", "http://localhost:8080"],
+//       connectSrc: ["'self'"],
+//       fontSrc: ["'self'", "https:", "data:"],
+//       objectSrc: ["'none'"],
+//       mediaSrc: ["'self'"],
+//       frameSrc: ["'none'"],
+//     },
+//   })
+// );
+
+app.use("/public", express.static("public"));
+app.use("/views", express.static("views"));
 
 mongoose
   .connect(process.env.MONGO_URI, {
