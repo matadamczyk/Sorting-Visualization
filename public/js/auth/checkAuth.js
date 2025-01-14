d618ec1d3df5e828986bf8cd25bf76684d148cce
Mateusz Adamczyk <madamczyk@student.agh.edu.pl>
2025-01-14 18:40:14 +0100

    feat: added vercel and fixed some bugs

---
+++ public/js/auth/checkAuth.js
@@ -0,0 +1,15 @@
+
+export function checkAuthStatus() {
+  const token = localStorage.getItem("token");
+  if (token) {
+    document.getElementById("signInButton").style.display = "none";
+    document.getElementById("signUpButton").style.display = "none";
+    document.getElementById("profile").style.display = "inline";
+    document.getElementById("logoutButton").style.display = "block";
+  } else {
+    document.getElementById("signInButton").style.display = "block";
+    document.getElementById("signUpButton").style.display = "block";
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("logoutButton").style.display = "none";
+  }
+}
